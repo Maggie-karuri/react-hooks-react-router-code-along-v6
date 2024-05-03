6bb28c102752d9c80998ba2e007148794ec2f905
Maggie-karuri <karurimargaret111@gmail.com>
2024-05-03 20:35:35 +0300

    React Router

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+function About() {
+    return (
+        <>
+        <header>
+            {/* Save space for NavBar */}
+        </header>
+        <main>
+            <h1>This is my component!</h1>
+
+        </main>
+        </>
+    )
+}
+export default About;
